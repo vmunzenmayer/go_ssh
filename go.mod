0e73d4f7f81310d1f45db6b9937817eea16eed24
Victor Munzenmayer <victor.munzenmayer@peanuthub.cl>
2019-06-22 22:33:34 -0400

    Copy files from remote path to local data folder

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/vmunzenmayer/go_ssh
 
 go 1.12
 
-require golang.org/x/crypto v0.0.0-20190621222207-cc06ce4a13d4
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/pkg/sftp v1.10.0
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190621222207-cc06ce4a13d4
+)
--- main.go
+++ main.go
@@ -3,19 +3,26 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
 
+	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 )
 
 const sshUser = ""
 const sshPass = ""
 const domainWithPort = "" // ex: google.com:22
+const remotePath = "/var/www/html/"
 
 func main() {
-	executeCommandPOC()
+	files := getTxtFilesFromRemoteServer()
+	getFilesFromRemoteServer(files)
 }
 
-func executeCommandPOC() {
+func getTxtFilesFromRemoteServer() []string {
 	conn := getConnectionSSH()
 	defer conn.Close()
 
@@ -27,15 +34,60 @@ func executeCommandPOC() {
 
 	var b bytes.Buffer
 	session.Stdout = &b
-	if err := session.Run("cd /var/www/html && ls"); err != nil {
+	if err := session.Run(`cd ` + remotePath + ` && ls *.txt`); err != nil {
 		fmt.Println("Failed to run: " + err.Error())
 	}
 
-	if b.Len() == 0 {
-		fmt.Println("No data!")
+	files := strings.Split(b.String(), "\n")
+	return files
+}
+
+func getFilesFromRemoteServer(files []string) {
+	conn := getConnectionSSH()
+	defer conn.Close()
+
+	client, err := sftp.NewClient(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	dstPath, _ := os.Getwd()
+	pathErr := os.MkdirAll(strings.Join([]string{dstPath, "data"}, "/"), 0777)
+
+	if pathErr != nil {
+		fmt.Println(pathErr)
 	}
 
-	fmt.Println(b.String())
+	for _, file := range files {
+		if len(file) > 0 {
+			// create destination file
+			dstFile, err := os.Create(strings.Join([]string{dstPath, "data", file}, "/"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer dstFile.Close()
+
+			// open source file
+			srcFile, err := client.Open(remotePath + file)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// copy source file to destination file
+			bytes, err := io.Copy(dstFile, srcFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%s: %d bytes copied\n", file, bytes)
+
+			// flush in-memory copy
+			err = dstFile.Sync()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 }
 
 func getConnectionSSH() *ssh.Client {
